0c2c78395a83d09048fb25d31900d760689b9b75
saidnahum <70162272+saidnahum@users.noreply.github.com>
2021-11-28 20:46:59 -0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
